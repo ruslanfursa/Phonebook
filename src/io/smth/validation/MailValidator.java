3db632f9cd515a36556c5b0458e263573f966a43
Ruslan Fursa <fursa_1986@mail.ru>
2023-01-27 20:25:38 +0300

    Refactor after code review

--- src/io/smth/validation/MailValidator.java
+++ src/io/smth/validation/MailValidator.java
@@ -3,6 +3,8 @@ package io.smth.validation;
 import java.util.regex.Pattern;
 
 public class MailValidator implements Validator{
+    private final static int MIN_LENGTH = 8;
+    private final static int MAX_LENGTH = 25;
     @Override
     public boolean validate(String s) {
        if(!isNullEmptyLength(s)){
@@ -11,7 +13,7 @@ public class MailValidator implements Validator{
         return AreAllSignsCorrect(s);
     }
     private boolean isNullEmptyLength(String s){
-        if(s == null || s.length() < 8 || s.length() > 25){
+        if(s == null || s.length() < MIN_LENGTH || s.length() > MAX_LENGTH){
             System.out.println("Email must contain min 8 signs, max 26 signs and must be not null");
             return false;
         }
--- src/io/smth/validation/SurnameValidator.java
+++ src/io/smth/validation/SurnameValidator.java
@@ -3,6 +3,9 @@ package io.smth.validation;
 import java.util.regex.Pattern;
 
 public class SurnameValidator implements Validator {
+    private final static int MIN_LENGTH = 1;
+    private final static int MAX_LENGTH = 15;
+
     @Override
     public boolean validate(String s) {
         if (!isNullEmptyLength(s)) {
@@ -14,7 +17,7 @@ public class SurnameValidator implements Validator {
     }
 
     private boolean isNullEmptyLength(String s) {
-        if (s == null || s.length() < 1 || s.length() > 15) {
+        if (s == null || s.length() < MIN_LENGTH || s.length() > MAX_LENGTH) {
             System.out.println("Surname must contain min 1 letter, max 16 letters and must be not null");
             return false;
         }
